083ef61d492ed016082f3cf16e8f5af8480ea455
vasukondreddy <vasudevareddykondreddy@gmail.com>
2025-02-12 22:58:04 +0530

    requirements updated

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
-
+import os
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
